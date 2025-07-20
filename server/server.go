6b2d671f7ff016865fd3534849d333b33572189f
tosuke <13393900+tosuke@users.noreply.github.com>
2025-07-21 01:32:00 +0900

    refactor(server): simplify Mount

--- server/server.go
+++ server/server.go
@@ -37,18 +37,15 @@ func (s *Server) Version(ctx context.Context, req *providerv1alpha1.VersionReque
 	}, nil
 }
 
-//nolint:funlen
 func (s *Server) Mount(ctx context.Context, req *providerv1alpha1.MountRequest) (*providerv1alpha1.MountResponse, error) {
-	targetPath := req.GetTargetPath()
-	if targetPath == "" {
-		return nil, status.Error(codes.InvalidArgument, "targetPath is required")
+	var permission fs.FileMode = 0o644 // Default permission
+	if permStr := req.GetPermission(); permStr == "" {
+		permu64, err := strconv.ParseUint(req.GetPermission(), 10, 32)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "unable to parse permission: %v", permStr)
+		}
+		permission = fs.FileMode(permu64)
 	}
-
-	permu64, err := strconv.ParseUint(req.GetPermission(), 10, 32)
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "unable to parse permission: %v", req.GetPermission())
-	}
-	permission := fs.FileMode(permu64)
 
 	cfg, err := config.ParseMountRequest(req.GetAttributes())
 	if err != nil {
@@ -58,34 +55,12 @@ func (s *Server) Mount(ctx context.Context, req *providerv1alpha1.MountRequest) 
 	ovs := make([]*providerv1alpha1.ObjectVersion, 0, len(cfg.Secrets))
 	files := make([]*providerv1alpha1.File, 0, len(cfg.Secrets))
 	for _, secret := range cfg.Secrets {
-		secretOp := sacloudsm.NewSecretOp(s.client, secret.VaultID)
-		unveilRequest := sacloudsmv1.Unveil{
-			Name: secret.Name,
-		}
-
-		// Set version if specified
-		if secret.Version != nil {
-			unveilRequest.SetVersion(sacloudsmv1.NewOptNilInt(*secret.Version))
-		}
-
-		unveilResult, err := secretOp.Unveil(ctx, unveilRequest)
+		ov, file, err := retrieveSecret(ctx, s.client, permission, &secret)
 		if err != nil {
-			return nil, status.Errorf(codes.Internal, "failed to unveil secret %q in vault %q: %v", secret.Name, secret.VaultID, err)
+			return nil, err
 		}
-
-		var version string
-		if ver, ok := unveilResult.GetVersion().Get(); ok {
-			version = strconv.FormatInt(int64(ver), 10)
-		}
-		ovs = append(ovs, &providerv1alpha1.ObjectVersion{
-			Id:      secret.ID(),
-			Version: version,
-		})
-		files = append(files, &providerv1alpha1.File{
-			Path:     secret.Name,
-			Mode:     int32(permission),
-			Contents: []byte(unveilResult.GetValue()),
-		})
+		ovs = append(ovs, ov)
+		files = append(files, file)
 	}
 
 	return &providerv1alpha1.MountResponse{
@@ -93,4 +68,37 @@ func (s *Server) Mount(ctx context.Context, req *providerv1alpha1.MountRequest) 
 		Error:         nil,
 		Files:         files,
 	}, nil
+}
+
+func retrieveSecret(
+	ctx context.Context, client *sacloudsmv1.Client, permission fs.FileMode, secret *config.Secret,
+) (*providerv1alpha1.ObjectVersion, *providerv1alpha1.File, error) {
+	secretOp := sacloudsm.NewSecretOp(client, secret.VaultID)
+
+	unveilRequest := sacloudsmv1.Unveil{
+		Name: secret.Name,
+	}
+	if secret.Version != nil {
+		unveilRequest.SetVersion(sacloudsmv1.NewOptNilInt(*secret.Version))
+	}
+
+	unveilResult, err := secretOp.Unveil(ctx, unveilRequest)
+	if err != nil {
+		return nil, nil, status.Errorf(codes.Internal, "failed to unveil secret %q in vault %q: %v", secret.Name, secret.VaultID, err)
+	}
+
+	var version string
+	if ver, ok := unveilResult.GetVersion().Get(); ok {
+		version = strconv.Itoa(ver)
+	}
+	ov := &providerv1alpha1.ObjectVersion{
+		Id:      secret.ID(),
+		Version: version,
+	}
+	file := &providerv1alpha1.File{
+		Path:     secret.Name,
+		Mode:     int32(permission),
+		Contents: []byte(unveilResult.GetValue()),
+	}
+	return ov, file, nil
 }
